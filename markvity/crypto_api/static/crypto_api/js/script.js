e6884d474a3bb0135e6eb8cb5dbd5cb96ec788eb
Abhinav Sharma <as567652@gmail.com>
2021-08-21 21:19:19 +0530

    Added More Info Button With Popup Window

--- markvity/crypto_api/static/crypto_api/js/script.js
+++ markvity/crypto_api/static/crypto_api/js/script.js
@@ -1,3 +1,6 @@
+let starting = 1;
+let count = 20;
+
 function show_page(page){
     if (page == 'page1'){
         document.querySelector(`#page1`).style.display = 'block';
@@ -8,6 +11,42 @@ function show_page(page){
         document.querySelector(`#page1`).style.display = 'none';
     }
 }
+
+document.addEventListener('click', event => {
+    const element = event.target;
+    if (element.id === 'more_info') {
+        const val = element.dataset.button_id
+        fetchWithRetry(`http://api.coincap.io/v2/assets?search=${val}`, 10).then(function (json) {
+            data = json;
+            document.querySelector("#popup").style.display = "block";
+            document.querySelector("#popup_header").innerHTML = `
+                <span id="close">&times;</span>
+                <h2>${data.data[0].name}</h2>`
+            document.querySelector("#popup_body").innerHTML = `
+            <h3>Rank :- </h3><p>${data.data[0].rank}</p>
+            <h3>Symbol :- </h3><p>${data.data[0].symbol}</p>
+            <h3>Name :- </h3><p>${data.data[0].name}</p>
+            <h3>Availaible Supply :- </h3><p>${data.data[0].supply}</p>
+            <h3>Total Quantity Of Asset Issued) :- </h3> <p>${data.data[0].maxSupply}</p>
+            <h3>MarketCap :- </h3><p>${data.data[0].marketCapUsd}</p>
+            <h3>Volume Last 24Hr :- </h3><p>${data.data[0].volumeUsd24Hr}</p>
+            <h3>Price :- </h3><p>${data.data[0].priceUsd}</p>
+            <h3>Change Percent In Last 24Hr :- </h3><p>${data.data[0].changePercent24Hr}</p>
+            <h3>Volume Weighted Average Price in the last 24 hours :- </h3><p>${data.data[0].vwap24Hr}</p>
+            `
+        })
+        .catch(function (err) {
+            console.log(`There was a problem with the fetch operation: ${err.message}`);
+        })
+    }
+    else if(element.id === 'close'){
+        document.querySelector("#popup").style.display = "none";
+    }
+    else if(element.id === 'popup'){
+        document.querySelector("#popup").style.display = "none";
+    }
+});
+
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#page2').style.display = 'none';
@@ -29,6 +68,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     load_trending();
 
+    submit_button = document.querySelector('#submit');
+    submit_button.disabled = true;  
+    document.querySelector('#search_form').onkeyup = () => {
+        if (document.querySelector('#searched_value').value.length == 0){
+            submit_button.disabled = true;
+        }
+        else{
+            submit_button.disabled = false;
+        }    
+    }
     document.querySelector('#search_form').onsubmit = function() {
         const search_keywd = document.querySelector('#searched_value').value;
         fetchWithRetry(`http://api.coincap.io/v2/assets?search=${search_keywd}`, 10).then(function (json) {
@@ -38,8 +87,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
             }
             else{
-                addTable(data, 'search_table');
+                addTable(data, 'search_table', true);
             }
+            document.querySelector('#searched_value').value = ""
+            submit_button.disabled = true;
         })
         .catch(function (err) {
             console.log(`There was a problem with the fetch operation: ${err.message}`);
@@ -50,6 +101,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function load_trending(){
+    starting = 1;
+
     document.querySelector(`#trend`).style.display = 'block';
     document.querySelector(`#search`).style.display = 'none';
     document.querySelector(`#all`).style.display = 'none';
@@ -57,7 +110,7 @@ function load_trending(){
 
     fetchWithRetry('http://api.coincap.io/v2/assets?limit=10', 10).then(function (json) {
         data = json;
-        addTable(data, 'trending_table');
+        addTable(data, 'trending_table', true);
     })
     .catch(function (err) {
         console.log(`There was a problem with the fetch operation: ${err.message}`);
@@ -77,10 +130,24 @@ function load_all(){
     document.querySelector(`#all`).style.display = 'block';
     document.querySelector(`#calculate`).style.display = 'none';
 
-    document.querySelector('#content').innerHTML = '<h1>All Crypto</h1>';
+    fetchWithRetry(`http://api.coincap.io/v2/assets?limit=${count}&offset=${starting - 1}`, 10).then(function (json) {
+        data = json;
+        addTable(data, 'all_table', false);
+        starting += count;
+        window.onscroll = () => {
+            if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+                load_all();
+            }
+        };
+    })
+    .catch(function (err) {
+        console.log(`There was a problem with the fetch operation: ${err.message}`);
+    })
 }
 
 function load_calculate(){
+    starting = 1;
+
     document.querySelector(`#trend`).style.display = 'none';
     document.querySelector(`#search`).style.display = 'none';
     document.querySelector(`#all`).style.display = 'none';
@@ -115,11 +182,11 @@ function convertToInternationalCurrencySystem (labelValue) {
     : Math.abs(Number(labelValue));
 }
 
-function addTable(data, id, extend = false){
+function addTable(data, id, empty){
     var tabl = document.getElementById(id);
     const l = document.getElementsByTagName('tr').length;
     
-    if (extend == false){
+    if (empty == true || starting == 1){
         tabl.innerHTML = `
         <tr>
             <th>Rank</th>
@@ -146,6 +213,6 @@ function addTable(data, id, extend = false){
         }
 
         var cell = row.insertCell(5);
-        cell.innerHTML = `<button data-button_id = ${data.data[i].id}>More Info</button>`;
+        cell.innerHTML = `<button data-button_id = ${data.data[i].id} id = "more_info">More Info</button>`;
     }
 }
